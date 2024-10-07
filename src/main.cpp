67770191c828a3c5b7708edc4518a7914e813575
de-passage <contact@sylvainleclercq.com>
2024-10-07 11:33:42 +0200

    Basic types

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,55 @@
+#include "types.hpp"
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <variant>
 #include <vector>
-#include <algorithm>
-#include "types.hpp"
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+struct game {
+  resource_t resources;
+  std::vector<transportation> travel_routes;
+  std::vector<pod> pods;
+  std::vector<landing_pad> landing_pads;
+  std::vector<building> buildings;
+};
 
-int main()
-{
 
-    // game loop
-    while (1) {
-        int resources;
-        cin >> resources; cin.ignore();
-        int num_travel_routes;
-        cin >> num_travel_routes; cin.ignore();
-        for (int i = 0; i < num_travel_routes; i++) {
-            int building_id_1;
-            int building_id_2;
-            int capacity;
-            cin >> building_id_1 >> building_id_2 >> capacity; cin.ignore();
-        }
-        int num_pods;
-        cin >> num_pods; cin.ignore();
-        for (int i = 0; i < num_pods; i++) {
-            string pod_properties;
-            getline(cin, pod_properties);
-        }
-        int num_new_buildings;
-        cin >> num_new_buildings; cin.ignore();
-        for (int i = 0; i < num_new_buildings; i++) {
-            string building_properties;
-            getline(cin, building_properties);
-        }
+template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+std::istream &operator>>(std::istream &in, game &g) {
+    in >> g.resources;
+    in.ignore();
+    g.travel_routes.clear();
+    in >> g.travel_routes;
+    in.ignore();
+    in >> g.pods;
+    in.ignore();
 
-        cout << "TUBE 0 1;TUBE 0 2;POD 42 0 1 0 2 0 1 0 2" << endl; // TUBE | UPGRADE | TELEPORT | POD | DESTROY | WAIT
+    std::vector<any_building> buildings;
+    in >> buildings;
+    for (auto &building : buildings) {
+      std::visit(overloaded{[&](landing_pad &&b) {
+                              g.landing_pads.push_back(std::move(b));
+                            },
+                            [&](struct building &&b) {
+                              g.buildings.push_back(std::move(b));
+                            }},
+                 std::move(building));
     }
+    in.ignore();
+    return in;
+}
+
+int main() {
+  game g;
+
+  // game loop
+  while (1) {
+
+    cout << "TUBE 0 1;TUBE 0 2;POD 42 0 1 0 2 0 1 0 2"
+         << endl; // TUBE | UPGRADE | TELEPORT | POD | DESTROY | WAIT
+  }
 }
--- src/types.hpp
+++ src/types.hpp
@@ -1,19 +1,109 @@
 #pragma once
 
-#include <strong_types.hpp>
+#include "strong_types.hpp"
+#include <istream>
+#include <variant>
 #include <vector>
 
-using building_id =
-    dpsg::strong_types::strong_value<int, struct building_tag,
-                                     dpsg::strong_types::comparable>;
+namespace custom_modifiers {
+namespace st = dpsg::strong_types;
+namespace meta = st::black_magic;
+
+struct streamable {
+  template <class T>
+  struct type
+      : meta::for_each<
+            meta::tuple<meta::tuple<st::shift_left, std::basic_istream<char>>,
+                        meta::tuple<st::shift_right, std::basic_ostream<char>>>,
+            meta::apply<st::implement_binary_operation, T>> {};
+};
+} // namespace custom_modifiers
+
+#define DEFINE_ID_TYPE(identifier)                                             \
+  using identifier =                                                           \
+      dpsg::strong_types::strong_value<int, struct identifier##_tag,           \
+                                       dpsg::strong_types::comparable,         \
+                                       custom_modifiers::streamable>;
+
+DEFINE_ID_TYPE(building_id);
+DEFINE_ID_TYPE(pod_id)
+
+using capacity_t = dpsg::strong_types::number<int, struct capacity_tag,
+                                              custom_modifiers::streamable>;
+using building_type =
+    dpsg::strong_types::strong_value<unsigned int, struct building_type_tag,
+                                     custom_modifiers::streamable,
+                                     dpsg::strong_types::comparable_with<int>>;
+
+using resource_t = dpsg::strong_types::number<int, struct resource_tag,
+                                              custom_modifiers::streamable>;
+
+constexpr static inline building_type landing_pad_type{0};
 
 struct transportation {
   building_id start_building;
   building_id end_building;
-  int capacity;
+  capacity_t capacity;
 };
 
+inline std::istream &operator>>(std::istream &in, transportation &t) {
+  return in >> t.start_building >> t.end_building >> t.capacity;
+}
+
 struct pod {
-  int identifier;
+  pod_id identifier;
   std::vector<building_id> stops;
 };
+
+template<class T>
+inline std::istream& operator>>(std::istream& in, std::vector<T>& vec) {
+  int size;
+  in >> size;
+  vec.resize(size);
+  for (auto &stop : vec) {
+    in >> stop;
+  }
+  return in;
+}
+
+inline std::istream &operator>>(std::istream &in, pod &p) {
+  return in >> p.identifier >> p.stops;
+}
+
+struct coordinates {
+  int x;
+  int y;
+};
+
+inline std::istream &operator>>(std::istream &in, coordinates &c) {
+  return in >> c.x >> c.y;
+}
+
+struct building {
+  building_id identifier;
+  building_type type;
+  coordinates coords;
+};
+
+struct landing_pad : building {
+  std::vector<building_type> expected_workers;
+};
+
+using any_building = std::variant<building, landing_pad>;
+
+inline std::istream &operator>>(std::istream &in, any_building &b) {
+  building_type type;
+  in >> type;
+  if (type == 0) {
+    landing_pad land;
+    land.type = type;
+    in >> land.identifier >> land.coords >> land.expected_workers;
+    b = land;
+  } else {
+    building mod;
+    mod.type = type;
+    in >> mod.identifier >> mod.coords;
+    b = mod;
+  }
+  return in;
+}
